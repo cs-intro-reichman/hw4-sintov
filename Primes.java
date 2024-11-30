223d80c6ef3abc579cb8af0eab4e7448b437f41b
Or Sintov <sintov@Ors-Laptop.local>
2024-11-30 10:22:39 +0200

    first try primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,33 @@
 public class Primes {
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        boolean[] findPrime = new boolean[n+1];
+        findPrime[0] = false;
+        findPrime[1] = false;
+        for (int i=2; i<findPrime.length; i++){
+            findPrime[i] = true;
+        }
+        int primeCheck = 2;
+        int currentLocation = 2;
+        int countPrimes = 0;
+        System.out.println("prime numbers up to " + n + ":");
+        while (primeCheck < findPrime.length){
+            if (findPrime[primeCheck]) {
+                System.out.println(primeCheck);
+                countPrimes ++;
+                while (currentLocation < findPrime.length) {
+                    if (currentLocation % primeCheck == 0){
+                        findPrime[currentLocation] = false;
+                    }
+                    currentLocation ++;
+                }
+            }
+            primeCheck++;
+            currentLocation = primeCheck;
+        }
+        double perPrime = ((double)countPrimes/(double)n)*100.0;
+        System.out.println("There are " + countPrimes + " prime numbers between 2 and " + n + " (" + (int)perPrime + "% are primes)");
+        
         // Replace this statement with your code
     }
 }

0ad3bd38bc051a9e456a2c3d39f6ff00f373746b
Or Sintov <sintov@Ors-Laptop.local>
2024-11-30 02:18:12 +0200

    chars second try

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -164,16 +164,21 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
         for (int i =0; i< Math.min(str1.length(), str2.length()); i++){
-            if (str1.charAt(i) < str2.charAt(i)){
-                return -1;
-            }else{
-                if (str2.charAt(i) < str1.charAt(i)) {
-                    return 1;
+            if(((str1.charAt(i)>= 65 && str1.charAt(i)<=90) ||
+             (str1.charAt(i) >= 97 && str1.charAt(i) <=122)) &&
+             ((str2.charAt(i)>= 65 && str2.charAt(i)<=90) ||
+             (str2.charAt(i) >= 97 && str2.charAt(i) <=122))){
+                if (str2.charAt(i) < str2.charAt(i)){
+                    return -1;
+                }else{
+                    if (str2.charAt(i) < str1.charAt(i)) {
+                        return 1;
+                    }
                 }
-            }
+             }else{
+                return -2;
+             }
         }
         if (str1.length() == str2.length()){
             return 0;
